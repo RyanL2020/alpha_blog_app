f5ae53c464bcaa342c33076e9bc41c52878f4b75
RL2020 <loc.fitness1@gmail.com>
2022-09-12 14:04:38 -0400

    listed articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
 
-    # def index 
-    # end 
+    def index 
+        @articles = Article.all
+    end 
 
     def show
         @article = Article.find(params[:id])
